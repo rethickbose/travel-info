943616720e4c4ff0a3a3cee6402459c907fa0366
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-03-27 17:56:32 +0530

    init backend

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const config = require("./utils/config");
+const dbInterface = require("./controllers/dbInterface");
+
+const startServer = async () => {
+  const app = express();
+  dbInterface(app);
+  app.listen(config.PORT, err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Server is running on Port:", config.PORT);
+  });
+};
+
+startServer();
