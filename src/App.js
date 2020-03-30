b026805d6d8e8b051bd2c34575ccdd3216ef6477
rethickbose <49016071+rethickbose@users.noreply.github.com>
2020-03-30 11:41:21 +0530

    aaded /api/create_new_entry
    aaded /api/create_new_entry

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min.js';
+import axios from 'axios'
+
+
 
 import About from './pages/about';
 import Home from './pages/home';
 import Navbar from './components/navbar';
+
+axios.defaults.baseURL = 'http://localhost:4543';
 
 
 
