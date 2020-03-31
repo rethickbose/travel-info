e0072b6662b26d882f556396d0b3253f797c789b
rethickbose <49016071+rethickbose@users.noreply.github.com>
2020-03-31 18:49:03 +0530

    rendered update delete
    rendered update delete

--- src/App.js
+++ src/App.js
@@ -1,66 +1,44 @@
-import React, { useState,useEffect, Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import '../node_modules/materialize-css/dist/css/materialize.min.css';
-import '../node_modules/materialize-css/dist/js/materialize.min.js';
-import axios from 'axios'
+import React, { useState, useEffect, Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+import axios from "axios";
 
+import Search from "./pages/search";
+import GlobalInfo from "./pages/about";
+import Home from "./pages/home";
+import Navbar from "./components/navbar";
+import Alter from './pages/alter'
 
-import Search from './pages/search'
-import GlobalInfo from './pages/about';
-import Home from './pages/home';
-import Navbar from './components/navbar';
+axios.defaults.baseURL = "http://localhost:4543";
 
-axios.defaults.baseURL = 'http://localhost:4543';
+const App = () => {
+  const [posts, setPosts] = useState([]);
 
-
-
- const App = () =>{
-
-   const [posts,setPosts] = useState([]);
-
-   useEffect(
-    ()=>{
+  useEffect(() => {
     axios
-    .get("http://localhost:4543/api/cities")
-    .then(res=> setPosts(res.data))
-    .catch(error=>console.log(error))
-    }
+      .get("http://localhost:4543/api/cities")
+      .then(res => setPosts(res.data))
+      .catch(error => console.log(error));
+  });
 
-   );
-
-   const [query,setQuery] = useState([]);
-
-   useEffect(
-    ()=>{
-    axios
-    .post("http://localhost:4543/api/city")
-    .then(res=> setQuery(res.data))
-    .catch(error=>console.log(error))
-    }
-
-   );
-
-   
-
-
-
-
-
-  return(
-     <Router>
-        <Navbar />
-        <div className='container'>
-            <Route exact path="/" component={Home} /> 
-            <Route exact path="/globalinfo" render={ ()=><GlobalInfo posts={posts} />} /> 
-            <Route exact path="/search" render={     ()=><Search  /> } />
-        </div>
-        
+  return (
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/globalinfo"
+          render={() => <GlobalInfo posts={posts} />}
+        />
+        <Route exact path="/search" render={() => <Search />} />
+        <Route exact path="/alter" component={Alter} />
+      </div>
     </Router>
-  )
-
- }
-
+  );
+};
 
 export default App;
